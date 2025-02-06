8a90d4bf8b6fc051a8670d4fdcae1081f01af523
Pavel Eremeev <pasha.eremeev.58@gmail.com>
2025-02-07 00:23:54 +0300

    перенос логики работы горизонтального календаря в view model и добавление date picker

--- app/src/main/java/com/example/eventposter/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/eventposter/app/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eventposter.app.CalendarClickListener
+import com.example.eventposter.app.ui.DatePickerFragment
 import com.example.eventposter.app.ui.adapters.recycler.CalendarAdapter
 import com.example.eventposter.app.ui.adapters.recycler.EventAdapter
 import com.example.eventposter.databinding.FragmentHomeBinding
@@ -28,6 +29,7 @@ class HomeFragment : Fragment() {
 
     companion object {
         private var fragment: HomeFragment? = null
+        private const val DATE_PICKER = "DATE_PICKER"
         fun getInstance(): HomeFragment {
             if (fragment == null) {
                 fragment = HomeFragment()
@@ -36,8 +38,7 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private lateinit var rvEventsPreviews: RecyclerView
-    private lateinit var  rvCalendarDays: RecyclerView
+    private lateinit var vm: HomeViewModel
     private lateinit var adapterCalendar: CalendarAdapter
 
     override fun onCreateView(
@@ -45,37 +46,36 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this)[HomeViewModel::class.java]
+        vm = ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        rvEventsPreviews = binding.rvEventPreviews
-        rvCalendarDays = binding.rvCalendarDays
-
         adapterCalendar = CalendarAdapter(requireContext(), object: CalendarClickListener {
             override fun invoke(date: Date) {
-                displayDate(date)
-                adapterCalendar.setSelected(date)
+                vm.setSelectedDate(date)
             }
         })
-        val calendar = Calendar.getInstance()
-        displayDate(calendar.time)
-        val lengthOfDays = 90
-        val dates = mutableListOf<Date>()
-        for (i in 0 until lengthOfDays) {
-            dates.add(calendar.time)
-            calendar.add(Calendar.DATE, 1)
-        }
-        adapterCalendar.days = dates
-        rvCalendarDays.adapter = adapterCalendar
-        rvCalendarDays.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+
+        binding.rvCalendarDays.adapter = adapterCalendar
+        binding.rvCalendarDays.addOnScrollListener(object: RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 updateMonthYear()
             }
         })
+
+        vm.selectedDate.observe(viewLifecycleOwner) { date ->
+            adapterCalendar.setSelected(date)
+            displayDate(date)
+        }
+
+        displayDate(Calendar.getInstance().time)
+
+        vm.days.observe(viewLifecycleOwner) { days ->
+            adapterCalendar.days = days
+            vm.selectedDate.value?.let { adapterCalendar.setSelected(it) }
+        }
 
         val adapterPreviews = EventAdapter(requireContext())
         adapterPreviews.events.add(
@@ -114,7 +114,15 @@ class HomeFragment : Fragment() {
                 null
             )
         )
-        rvEventsPreviews.adapter = adapterPreviews
+        binding.rvEventPreviews.adapter = adapterPreviews
+
+        binding.btnDatePicker.setOnClickListener {
+            DatePickerFragment().apply {
+                setListener { date ->
+                    vm.setSelectedDate(date)
+                }
+            }.show(parentFragmentManager, DATE_PICKER)
+        }
 
         return root
     }
@@ -124,11 +132,11 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    fun updateMonthYear() {
-        val lm = rvCalendarDays.layoutManager as LinearLayoutManager
+    private fun updateMonthYear() {
+        val lm = binding.rvCalendarDays.layoutManager as LinearLayoutManager
         val currentPos = lm.findFirstVisibleItemPosition()
         if (currentPos != RecyclerView.NO_POSITION) {
-            val visibleDay = (rvCalendarDays.adapter as CalendarAdapter).days[currentPos]
+            val visibleDay = (binding.rvCalendarDays.adapter as CalendarAdapter).days[currentPos]
             val cal = Calendar.getInstance()
             cal.time = visibleDay
             val fmtMonthYear = SimpleDateFormat("LLLL yyyy", Locale("ru"))
@@ -141,7 +149,7 @@ class HomeFragment : Fragment() {
         }
     }
 
-    fun displayDate(date: Date) {
+    private fun displayDate(date: Date) {
         val fmt = SimpleDateFormat("d MMMM", Locale("ru", "RU"))
         binding.tvDate.text = fmt.format(date)
     }
--- app/src/main/java/com/example/eventposter/app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/eventposter/app/ui/home/HomeViewModel.kt
@@ -3,11 +3,38 @@ package com.example.eventposter.app.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.Calendar
+import java.util.Date
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    companion object {
+        const val LENGTH_OF_DAYS = 90
     }
-    val text: LiveData<String> = _text
+
+    private val _days = MutableLiveData<List<Date>>()
+    val days: LiveData<List<Date>>
+        get() = _days
+
+    private val _selectedDate = MutableLiveData<Date>()
+    val selectedDate: LiveData<Date>
+        get() = _selectedDate
+
+    init {
+        getDays()
+    }
+
+    fun setSelectedDate(date: Date) {
+        _selectedDate.value = date
+    }
+
+    private fun getDays() {
+        val calendar = Calendar.getInstance()
+        val dates = mutableListOf<Date>()
+        for (i in 0 until LENGTH_OF_DAYS) {
+            dates.add(calendar.time)
+            calendar.add(Calendar.DATE, 1)
+        }
+        _days.value = dates
+    }
 }
